0ad300c7f475940085b627836938db35a764eddd
unknown <victo@stark.win.cs.columbia.edu>
2019-01-16 14:05:09 -0500

    added project folder

---
+++ VictoriasProject/Assets/CollisionDetect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Enter called.");
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Stay occurring.");
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit called.");
+    }
+}
---
+++ VictoriasProject/Assets/OnCollisionTransform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionTransform : MonoBehaviour {
+
+    Vector3 previousPos;
+    Quaternion previousRot;
+
+    void OnCollisionStay(Collision col)
+    {
+        ViveControllerInput input = GetComponent<ViveControllerInput>();
+
+        Vector3 currentPos = transform.position;
+        Vector3 relativePos = (previousPos - currentPos);
+
+        Quaternion currentRot = transform.rotation;
+        Quaternion r12 = (Quaternion.Inverse(currentRot) * previousRot);
+
+        if (input.getPinch())
+        {
+            col.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            col.gameObject.transform.position -= relativePos;
+        }
+
+        if (!input.getPinch())
+            col.gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+        if (input.getTouchPad())
+            col.gameObject.transform.rotation *= r12;
+
+        if (input.getGrip())
+            col.gameObject.transform.localScale = relativePos;
+
+        previousPos = currentPos;
+        previousRot = currentRot;
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        col.gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
